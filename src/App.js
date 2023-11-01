b65ce26b6d90924442ac40598410fe6098b66439
Y.Evitha <evithay12@gmail.com>
2023-11-01 11:39:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css';
+import colorArray from './colors';
+import List from './List';
+
+function App() {
+  const [color, setColor] = useState(colorArray);
+  const [showList, setShowList] = useState(false);
+  const handleButtonClick = () => {
+    setShowList(!showList);
+  };
+
+  return (
+    <center>
+      <div id='container'>
+        {showList && <List colors={color}/>}
+      </div>
+        <p>Color Picker</p>
+      <h3 id="display"></h3>
+      <button onClick={handleButtonClick}>Pick a color</button>
+    </center>
+  );
+}
+
+export default App;
